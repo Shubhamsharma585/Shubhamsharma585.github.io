e02d0e437d3c5effb544692c667d0858b95a7eb8
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-07-11 13:08:27 +0530

    first co

---
+++ src/Compo/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from "./Home"
+
+
+
+function Router() {
+
+
+
+
+    return (
+        <div>
+            <Home/>
+        </div>
+    )
+}
+
+export default Router;
