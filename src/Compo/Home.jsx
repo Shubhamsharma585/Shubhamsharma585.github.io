94fc1df47a1edb8e2235379680667b81517bdc50
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-08-01 18:03:35 +0530

    updating css

--- src/Compo/Home.jsx
+++ src/Compo/Home.jsx
@@ -20,49 +20,16 @@ function Home() {
    
     return (
         <div id="home_top" className="home_top">
-         
-{/* 
-          <div className="home_banner">
 
-             <div >  
-                 <img src={ban} className="home_image"/>
-              </div>
-
-              <div className="home_profile">
-
-                <h1 className="home_tag">Full Stack Developer</h1>
-                <h1 className="home_name"><strong>Shubham Sharma</strong></h1>
-                <Typical                
-                   steps={[
-                    'An NITian',
-                    600,
-                    'GATE Qualified',
-                    600,
-                    'Frontend',
-                    800,
-                    'Backend', 
-                    800,
-                    'Data structures and algorithms',
-                    800,
-                  ]} 
-                   loop={Infinity}
-                   wrapper="p"
-                   className="home_subtag"
-                 /> 
-               <Link  to="projects" spy={true} smooth={true}> <div className="home_getstarted">
-                <p className="mywork">View my work</p>
-                    <p></p>
-                </div></Link>
-
-              </div>
-          </div> */}
 
 
           <div className="navbar2">
            <Nav/>
            <Top/>
            <About/>
+
            <Masai/>
+
            <Prof/> 
            <Project/>
            <Blog/>
